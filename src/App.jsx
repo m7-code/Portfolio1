dc828b2bf2cca1213375ae780507d372797b9058
m7-code <mughiraasad6@gmail.com>
2026-08-08 02:10:27 -0700

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Home from "./components/Home";
 import CursorDust from "./components/CursorDust";
-
+import ShatterPreloader from "./components/ShatterPreloader";
 function App() {
   return (
     <>
+      <ShatterPreloader />
       <CursorDust />
       <Home />
     </>
